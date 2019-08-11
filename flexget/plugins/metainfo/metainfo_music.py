90f4c55fb643f1b71cced01f48e71b6c5f2433bb
Thomas Riccardi <kiwiiii@gmail.com>
2019-08-12 01:13:36 +0200

    Add metainfo_music parser: artist, album, year, using guessit
    TODO:
    - quality (for now it's metainfo_quality that fills it...)
    - support simple titles: "artist : album" is wrongly parsed as
    everything in the "artist"
    - more types? album, ep, track, collection, discography...
    - proper quality matching dedicated for audio (TR24, 320kbps mp3...)
    - maybe music_list type to easily add/remove on artist+album (+
    optional year when we have it)

---
+++ flexget/plugins/metainfo/metainfo_music.py
@@ -0,0 +1,45 @@
+from loguru import logger
+
+from flexget import plugin
+from flexget.event import event
+
+logger = logger.bind(name='metainfo_music')
+
+
+class MetainfoMusic:
+    """Assume entry is an album, and populate music info."""
+
+    schema = {'type': 'boolean'}
+
+    def on_task_metainfo(self, task, config):
+        # Don't run if we are disabled
+        if config is False:
+            return
+        for entry in task.entries:
+            # If music parser already parsed this, don't touch it.
+            if entry.get('music_artist'):
+                continue
+            self.guess_entry(entry)
+
+    @staticmethod
+    def guess_entry(entry):
+        """Populate music_* fields for entries that are successfully parsed.
+
+        :param entry: Entry that's being processed
+        :return: True for successful parse
+        """
+        if entry.get('music_guessed'):
+            # Return true if we already parsed this
+            return True
+        parser = plugin.get('parsing', 'metainfo_music').parse_music(data=entry['title'])
+        if parser and parser.valid:
+            for field, value in parser.fields.items():
+                if not entry.is_lazy(field) and not entry.get(field):
+                    entry[field] = value
+            return True
+        return False
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(MetainfoMusic, 'metainfo_music', api_ver=2)
