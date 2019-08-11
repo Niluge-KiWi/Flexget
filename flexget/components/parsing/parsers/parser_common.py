537b8e8d31c7678937026e2568e3daedf5af694c
Thomas Riccardi <kiwiiii@gmail.com>
2019-08-12 01:13:36 +0200

    Add metainfo_music parser: artist, album, year, using guessit
    TODO:
    - quality (for now it's metainfo_quality that fills it...)
    - support simple titles: "artist : album" is wrongly parsed as
    everything in the "artist"
    - more types? album, ep, track, collection, discography...
    - proper quality matching dedicated for audio (TR24, 320kbps mp3...)
    - maybe music_list type to easily add/remove on artist+album (+
    optional year when we have it)

--- flexget/components/parsing/parsers/parser_common.py
+++ flexget/components/parsing/parsers/parser_common.py
@@ -110,6 +110,58 @@ class MovieParseResult:
         )
 
 
+class MusicParseResult:
+    def __init__(
+        self,
+        data: Optional[str] = None,
+        artist: Optional[str] = None,
+        album: Optional[str] = None,
+        year: Optional[int] = None,
+        quality: Quality = None,
+        valid: bool = True,
+    ) -> None:
+        self.artist: str = artist
+        self.data: str = data
+        self.album: Optional[str] = album
+        self.year: Optional[int] = year
+        self.quality: Quality = quality if quality is not None else Quality()
+        self.valid: bool = valid
+
+    @property
+    def identifier(self):
+        if self.artist and self.album and self.year:
+            return (f'{self.artist} - {self.year} - {self.album}').strip().lower()
+        elif self.artist and self.album:
+            return (f'{self.artist} - {self.album}').strip().lower()
+        elif self.artist:
+            return self.artist.lower()
+
+    @property
+    def fields(self) -> dict:
+        """
+        Return a dict of all parser fields
+        """
+        return {
+            'id': self.identifier,
+            'music_parser': self,
+            'music_artist': self.artist,
+            'music_album': self.album,
+            'music_year': self.year,
+        }
+
+    def __str__(self) -> str:
+        valid = 'OK' if self.valid else 'INVALID'
+        return '<MusicParseResult(data={},artist={},album={},year={},id={},quality={},status={})>'.format(
+            self.data,
+            self.artist,
+            self.album,
+            self.year,
+            self.identifier,
+            self.quality,
+            valid,
+        )
+
+
 class SeriesParseResult:
     def __init__(
         self,
